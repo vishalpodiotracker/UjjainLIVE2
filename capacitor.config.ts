94c917208db71c214af791aa976f119336516951
vishalpodiotracker <vishal@podiotracker.com>
2025-02-22 11:47:25 +0530

    Fixed Errors

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ujjainlive.Ujjainlive',
   appName: 'Ujjainlive',
-<<<<<<< HEAD
   webDir: 'www/browser',
-=======
-  webDir: 'www',
->>>>>>> 0c5b296ce3c583aa7761df121029b24325a0254f
   bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
@@ -18,13 +14,9 @@ const config: CapacitorConfig = {
       androidSplashResourceName: "splash",
       androidScaleType: "CENTER_CROP",
       showSpinner: true,
-<<<<<<< HEAD
       spinnerColor: "#999999",
       androidSpinnerStyle: "large",
       iosSpinnerStyle: "small",
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
       splashFullScreen: true,
       splashImmersive: true,
       layoutName: "launch_screen",
